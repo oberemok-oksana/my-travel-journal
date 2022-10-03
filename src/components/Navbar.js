3c719219c8c7d40468590abba5b7886d2a1aac3f
Oksana Oberemok <Pylypenkok@gmail.com>
2022-10-03 20:14:25 +0800

    implement Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="nav">
+        <a className="logo" href="#">
+          <img
+            className="logo-img"
+            src="/images/globus.png"
+            alt="icon of globe"
+          />
+          My travel journal.
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
