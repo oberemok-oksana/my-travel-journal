512fa61e3fba4c69d694c9a76337094417017440
Oksana Oberemok <Pylypenkok@gmail.com>
2022-10-03 20:15:23 +0800

    add Navbar,Card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import data from "./data";
+import Card from "./components/Card";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="cards">
+        {data.map((item) => (
+          <Card
+            key={item.id}
+            location={item.location}
+            imageUrl={item.imageUrl}
+            googleMapsUrl={item.googleMapsUrl}
+            startDate={item.startDate}
+            endDate={item.endDate}
+            description={item.description}
+            title={item.title}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
